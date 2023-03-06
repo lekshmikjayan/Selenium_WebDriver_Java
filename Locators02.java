7655754c6e74adb3beae6af1d84ad3b66f7b8265
lekshmikjayan <lekshmikjayan27@gmail.com>
2023-03-06 06:54:19 -0500

    Advanced locator identification

---
+++ Locators02.java
@@ -0,0 +1,23 @@
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators02 {
+	public static void main (String[]args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:/Users/leksh/Downloads/chromedriver_win32/chromedriver.exe" );
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		//sibling to sibling
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		//child to parent
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
+
+	}
+
+}
