2df008902ef2b4a4c1a62357e1643e460f9d76dc
lekshmikjayan <lekshmikjayan27@gmail.com>
2023-03-08 06:58:05 -0500

    Modified

--- Base.java
+++ Base.java
@@ -1,35 +1,117 @@
-import java.time.Duration;
 import java.util.Arrays;
+
 import java.util.List;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Base {
-	public static void main(String[] args) {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", "C:/Users/leksh/Downloads/chromedriver_win32/chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
-		
-		String[] veggies = {Cucumber, Brocoli, Beetroot};
-		
+
+		// Implicit wait
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		WebDriverWait w = new WebDriverWait(driver, 5);
+
+		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot", "Tomato", "Beans" };
+
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+
+		Thread.sleep(3000);
+
+		addItems(driver, itemsNeeded);
+
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+
+		driver.findElement(By.xpath("//button[contains(text(), 'PROCEED TO CHECKOUT')]")).click();
 		
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		
+
+		// explicit wait
+
+		w.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("promoInfo")));
+
+		System.out.println(driver.findElement(By.className("promoInfo")).getText());
+		
+		driver.findElement(By.xpath("//button[text() = 'Place Order']")).click();
+		
+		WebElement staticDropdown = driver.findElement(By.xpath("//div/div/div/select"));
+		
+		Select dropdown = new Select(staticDropdown);
+		
+		dropdown.selectByIndex(17);
+		
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		
+		dropdown.selectByValue("Brazil");
+		
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		
+		driver.findElement(By.className("chkAgree")).click();
+		
+		driver.findElement(By.xpath("//button[contains(text(), 'Proceed')]")).click();
+		
+		
+	}
+
+	public static void addItems(WebDriver driver, String[] itemsNeeded)
+
+	{
+
+		int j = 0;
+
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 
-		for (int i = 0; i < products.size(); i++) {
+		for (int i = 0; i < products.size(); i++)
 
-			String name = products.get(i).getText().split('-');
-			
-			List itemsNeeded = Arrays.asList(veggies);
+		{
 
-			if (name.contains(name)) {
-				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-		
+			String[] name = products.get(i).getText().split("-");
+
+			String formattedName = name[0].trim();
+
+			List itemsNeededList = Arrays.asList(itemsNeeded);
+
+			if (itemsNeededList.contains(formattedName)) {
+
+				j++;
+
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+
+				if (j == itemsNeeded.length)
+
+				{
+
+					break;
+
+				}
+
 			}
+
 		}
+
 	}
+
 }
--- Locators01.java
+++ Locators01.java
@@ -4,21 +4,46 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Locators01{ 
 
 	public static void main(String[] args)throws InterruptedException {
-
+		String name = "Lekshmi";
 		System.setProperty("webdriver.chrome.driver","C:/Users/leksh/Downloads/chromedriver_win32/chromedriver.exe" );
 	
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String password = getPassword(driver);
+		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 
 		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Lekshmi");
-		driver.findElement(By.cssSelector("input[type='password']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("input[type='password']")).sendKeys(password);
 		driver.findElement(By.className("submit")).click();
 		
-		//p[text()='You are successfully logged in.']
+		Thread.sleep(2000);
+		System.out.println(driver.findElement(By.tagName("p")).getText());
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"You are successfully logged in.");
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),"Hello "+name+",");
+		driver.findElement(By.cssSelector("button[class='logout-btn']")).click();
+		driver.close();
+		
 	}
+	
+	public static String getPassword(WebDriver driver) throws InterruptedException {
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+		
+		String[] passwordArray = passwordText.split("'");
+		String password = passwordArray[1].split("'")[0];
+		return password;
+		
+	}
+	
 }
